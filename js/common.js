d399cd1a9df58e0cebf5293f63749e0a7130195f
SiteInSight <dev@site-insight.ru>
2019-04-16 17:28:24 +0300

    Верстка 85%

--- js/common.js
+++ js/common.js
@@ -2,4 +2,19 @@ $(function() {
 
 	// Custom JS
 
+	$('.grid').isotope({
+		// options
+		itemSelector: '.grid-item',
+		layoutMode: 'fitRows'
+	});
+
+	$('.grid').magnificPopup({
+		delegate: 'a', // child items selector, by clicking on it popup will open
+		type: 'image',
+		gallery: {
+      enabled: true
+    },
+		// other options
+	});
+
 });
